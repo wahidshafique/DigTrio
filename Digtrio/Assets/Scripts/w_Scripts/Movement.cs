f2bb5fb9c3c4c28c56832ac1eeb3a6c79246bffb
Wahid Shafique <wshafique@SCT.local>
2016-02-12 13:26:34 -0500

    added basic project structure, sticking to Master

---
+++ Digtrio/Assets/Scripts/w_Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    //TODO replace with touch
+    public float speed = 2.5f;
+    private Vector3 target;
+
+    void Start() {
+        target = transform.position;
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+            float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
