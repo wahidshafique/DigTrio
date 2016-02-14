dae666cd06cb327feafd91ecaf49fdf1060a92d8
CarloAlbino <carloalbino@gmail.com>
2016-02-14 00:06:43 -0500

    Getting started on enemy AI
    Wander and Seek working on the enemy character

---
+++ Digtrio/Assets/Scripts/c_scripts/EnemyMovement.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    #region Variables
+
+    public float speed = 1.0f;
+
+    private int ydir = 0;
+    private Vector3 lastPos;
+
+    private bool facingLeft = true;
+    private bool movingLeft = true;
+
+    public bool canWander = true;
+
+    public Transform target = null;
+
+    #endregion Variables
+
+    #region Monobehaviour
+
+    void Update () {
+        if (canWander)
+        {
+            Wander();
+        }
+        else
+        {
+            Seek();
+        }
+        FlipSprite();
+	}
+
+    #endregion Monobehaviour
+
+    #region Public Functions
+
+    public void SetSeek(Transform t)
+    {
+        target = t;
+        SetWander(false);
+    }
+
+    public void SetWander(bool b)
+    {
+        canWander = b;
+    }
+
+    #endregion Public Functions
+
+    #region Private Functions
+
+    private void Wander()
+    {
+        MoveForward();
+        MoveY(RandomBinomial());
+        if (Random.Range(-1, 20) < 0)
+        {
+            speed *= -1;
+        }
+    }
+
+    private void Seek()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, Mathf.Abs(speed) * Time.deltaTime);
+    }
+
+    private void FlipSprite()
+    {
+        if (transform.position.x - lastPos.x > 0)   // Moving Right
+        {
+            if (movingLeft && facingLeft)
+            {
+                Vector2 scale = transform.localScale;
+                scale.x *= -1.0f;
+                transform.localScale = scale;
+                facingLeft = false;
+                movingLeft = false;
+            }
+        }                                           // Moving Left
+        else
+        {
+            if (!movingLeft && !facingLeft)
+            {
+                Vector2 scale = transform.localScale;
+                scale.x *= -1.0f;
+                transform.localScale = scale;
+                facingLeft = true;
+                movingLeft = true;
+            }
+        }
+        lastPos = transform.position;
+    }
+
+    private void MoveForward()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    private void MoveY(int direction)
+    {
+        transform.Translate(Vector2.up * Mathf.Abs(speed) * 0.5f * direction * Time.deltaTime);
+    }
+
+    private int RandomBinomial()
+    {
+        return (Random.Range(0, 2) - Random.Range(0, 2));
+    }
+
+    #endregion Private Functions
+}
---
+++ Digtrio/Assets/Scripts/c_scripts/KinematicAIClasses.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Static
+{
+    public Vector2 position;
+    public float orientation;
+
+    public Vector2 OrientationAsVector()
+    {
+        Vector2 vector = new Vector2(Mathf.Sin(orientation), Mathf.Cos(orientation));
+        return vector;
+    }
+}
+
+public struct Kinematic
+{
+    public Vector2 position;
+    public float orientation;
+    public Vector2 velocity;
+    public float rotation;
+
+    public void UpdatePositon(KinematicSteeringOutput steering)
+    {
+        // Update the position and orientation of the enemy
+        position += velocity * Time.time + 0.5f * steering.velocity * Time.time * Time.time;
+        orientation += rotation * Time.time + 0.5f * steering.rotation * Time.time * Time.time;
+
+        // Update the velocity and the rotation of the enemy
+        velocity += steering.velocity * Time.time;
+        rotation += steering.rotation * Time.time;
+    }
+
+    public Vector2 OrientationAsVector()
+    {
+        Vector2 vector = new Vector2(Mathf.Sin(orientation), Mathf.Cos(orientation));
+        return vector;
+    }
+
+}
+
+public struct SteeringOutput
+{
+    public Vector3 linear;
+    public float angular;
+}
+
+public struct KinematicSteeringOutput
+{
+    public Vector2 velocity;
+    public float rotation;
+}
+
+public class KinematicSeek
+{
+    // Holds static data for the character and target
+    Static character;
+    Static target;
+
+    // Holds the maximum speed the character can travel
+    float maxSpeed;
+
+    public float GetNewOrientation(float currentOrientation, Vector3 velocity)
+    {
+        // Make sure we have a velocity
+        if (velocity.magnitude > 0)
+        {
+            // Calculate orientation using an arc tangent of the velocity components
+            return Mathf.Atan2(-velocity.x, velocity.z);
+        }
+        // Otherwise use the current orientation
+        else
+        {
+            return currentOrientation;
+        }
+    }
+
+    public KinematicSteeringOutput GetSteering()
+    {
+        // Create the structure for the output
+        KinematicSteeringOutput steering = new KinematicSteeringOutput();
+
+        // Get the direction to the target
+        steering.velocity = target.position - character.position;
+
+        // The velocity is along this direction, at full speed
+        steering.velocity.Normalize();
+        steering.velocity *= maxSpeed;
+
+        // Face in the direction we want to move
+        character.orientation = GetNewOrientation(character.orientation, steering.velocity);
+
+        // Output the steering
+        steering.rotation = 0;
+
+        return steering;
+    }
+}
+
+public class KinematicArrive : KinematicSeek
+{
+    // Holds the static data for the character and target
+    Static character;
+    Static target;
+
+    // Holds the maximum speed the character can travel
+    float maxSpeed;
+
+    // Holds the satisfaction radius
+    float radius;
+
+    // Holds the time to target constant
+    float timeToTarget = 0.25f;
+
+    public KinematicSteeringOutput GetSteering()
+    {
+        // Create the stucture for the output
+        KinematicSteeringOutput steering = new KinematicSteeringOutput();
+
+        // Get the direction to the target
+        steering.velocity = target.position - character.position;
+
+        // Check if we're within radius
+        if (steering.velocity.magnitude < radius)
+        {
+            // We can return no steering request
+            return steering;
+        }
+
+        // We need to move to our target, we'd like to get there in timeToTarget seconds
+        steering.velocity /= timeToTarget;
+
+        // If this is to fast, clip it to the max speed
+        if (steering.velocity.magnitude > maxSpeed)
+        {
+            steering.velocity.Normalize();
+            steering.velocity *= maxSpeed;
+        }
+
+        // Face in the direction we want to move
+        character.orientation = GetNewOrientation(character.orientation, steering.velocity);
+
+        // Output the steering
+        steering.rotation = 0;
+        return steering;
+    }
+}
+
+public class KinematicWander
+{
+    // Holds the static data for the character
+    Kinematic character;
+
+    // Holds the maximum speed the character can travel
+    float maxSpeed;
+
+    // Holds the maximum rotation speed we'd like, probably should be smaller than the maximum possible, 
+    // to allow a leisurely change in direction
+    float maxRotation;
+
+    public void SetWander(Kinematic c, float mS, float mR)
+    {
+        character = c;
+        maxSpeed = mS;
+        maxRotation = mR;
+    }
+
+    public KinematicSteeringOutput GetSteering()
+    {
+        // Create a structure for output
+        KinematicSteeringOutput steering = new KinematicSteeringOutput();
+
+        // Get velocity from the vector form of the orientation
+        steering.velocity = maxSpeed * character.OrientationAsVector();
+
+        // Change our orientation randomly
+        steering.rotation = (Random.Range(0,2) - Random.Range(0,2)) * maxRotation;
+
+        // Output the steering
+        return steering;
+    }
+}
