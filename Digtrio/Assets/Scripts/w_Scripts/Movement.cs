f6ab39cbb54aa3c8acb1e8efa4c8e9ef1b95b457
wahidshafique <wahidshafique@hotmail.com>
2016-02-15 21:13:28 -0500

    added movement + camera /redux

--- Digtrio/Assets/Scripts/w_Scripts/Movement.cs
+++ Digtrio/Assets/Scripts/w_Scripts/Movement.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Movement : MonoBehaviour {
     public float speed = 2.5f;
     private Vector3 target;
-    
+    Quaternion targetRot;
+
     void OnGUI() {
         foreach (Touch touch in Input.touches) {
             string message = "";
@@ -24,14 +25,26 @@ public class Movement : MonoBehaviour {
     }
 
     void Update() {
+        MoveToClick();
+        Sniff();
+    }
+
+    void MoveToClick() {
         if (Application.isEditor) {
             if (Input.GetMouseButtonDown(0)) {
                 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 target.z = transform.position.z;
-                float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                targetRot = Quaternion.LookRotation(target - transform.position, Vector3.forward);
+                targetRot.x = 0.0f;
+                targetRot.y = 0.0f;
             }
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * 8);
             transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
         }
+    }
+
+    void Sniff() {
+        RaycastHit2D foundHit = Physics2D.Raycast(transform.position, transform.right, 2, 1 << 8);
+        Debug.DrawRay(transform.position, target, Color.green, 2);
     }
 }
