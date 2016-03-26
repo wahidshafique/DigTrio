b025e53e38738a1c8160ca3d28f3eda3540f9788
CarloAlbino <carloalbino@gmail.com>
2016-03-26 15:46:38 -0400

    New Speech Bubble!

---
+++ Digtrio/Assets/Scripts/c_scripts/DynamicStretch.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DynamicStretch : MonoBehaviour {
+    public Transform A, B;
+    public Vector3 startPosition = new Vector3(0f, 0f, 0f);
+    public Vector3 endPosition = new Vector3(5f, 1f, 0f);
+    public bool mirrorZ = true;
+
+    public float xOffset = 1.0f;
+    public float yOffset = 1.0f;
+    private Vector3 APosition = Vector3.zero;
+
+    public float scaleOffset = 10.0f;
+
+    void Start()
+    {
+        UpdateAPosition();
+        startPosition = A.position;
+        endPosition = B.position;
+        Strech(gameObject, startPosition, endPosition, mirrorZ);
+    }
+
+    void Update()
+    {
+        UpdateAPosition();
+        startPosition = A.position;
+        endPosition = B.position;
+        Strech(gameObject, startPosition, endPosition, mirrorZ);
+    }
+
+    public void Strech(GameObject _sprite, Vector3 _initialPosition, Vector3 _finalPosition, bool _mirrorZ)
+    {
+        Vector3 centerPos = (_initialPosition + _finalPosition) / 2f;
+        _sprite.transform.position = centerPos;
+
+        Vector3 direction = _finalPosition - _initialPosition;
+        direction = Vector3.Normalize(direction);
+        _sprite.transform.right = direction;
+
+        if (_mirrorZ)
+            _sprite.transform.right *= -1f;
+
+        Vector3 scale = new Vector3(1, 1, 1);
+        scale.x = Vector3.Distance(_initialPosition, _finalPosition) * scaleOffset;
+        _sprite.transform.localScale = scale;
+    }
+
+    private void UpdateAPosition()
+    {
+        APosition.x = Camera.main.transform.position.x + xOffset;
+        APosition.y = Camera.main.transform.position.y + yOffset;
+        APosition.z = 0;
+
+        A.position = APosition;
+    }
+}
---
+++ Digtrio/Assets/Scripts/c_scripts/Speech_Canvas.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Speech_Canvas : MonoBehaviour {
+
+    public GameObject SpeechBubbleObject;
+
+    private string SpeechText;
+    public Text SpeechBubble;
+
+    public bool IsShowBubble = false;
+
+    void Update()
+    {
+        DisplayText();
+    }
+
+    public void DisplayText()
+    {
+        if (IsShowBubble)
+        {
+            SpeechBubbleObject.SetActive(true);
+            SpeechBubble.text = SpeechText;
+        }
+        else
+        {
+            SpeechBubbleObject.SetActive(false);
+        }
+    }
+
+    // Activate the bubble
+    public void ShowBubble(bool b)
+    {
+        IsShowBubble = b;
+        SpeechText = RandomSpeech();
+    }
+
+    public void SellItemsButton()
+    {
+        Inventory.Finder.SellItems();
+
+        // player prefs?
+
+        //Application.LoadLevel("Leaderboards");
+    }
+
+    // random speech
+    string RandomSpeech()
+    {
+        int rand = Random.Range(0, 7);
+        string randomSpeech = "";
+
+        switch (rand)
+        {
+            case 0:
+                randomSpeech = "\n Give me your goods.";
+                break;
+            case 1:
+                randomSpeech = "Time is of the essence, I'm too mole for this...";
+                break;
+            case 2:
+                randomSpeech = "I think I'm forgetting something. Can you dig it?";
+                break;
+            case 3:
+                randomSpeech = "If you don't bring me what you find, you won't get any points!";
+                break;
+            case 4:
+                randomSpeech = "Get a point multiplier by chaining the same items in a row.";
+                break;
+            case 5:
+                randomSpeech = "Hitting snakes will make you drop what you are carrying!";
+                break;
+            case 6:
+                randomSpeech = "If you are a friend, you speak the password, and the doors will open.";
+                break;
+            case 7:
+                randomSpeech = "I DRINK YOUR MILKSHAKE!";
+                break;
+        }
+        return randomSpeech;
+    }
+
+}
